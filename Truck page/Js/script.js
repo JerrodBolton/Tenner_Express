e4be85fd37e3ae19801ac2d150cea1ac781adb02
Jerrod <JerrodBolton2000@gmail.com>
2023-05-01 18:03:35 -0500

    added the project on there

--- Truck page/Js/script.js
+++ Truck page/Js/script.js
@@ -1,13 +1,21 @@
 //grabs the div holding the background image
 const bgImageEl = document.getElementById('bg-image')
-
+const vidEl = document.getElementById("smoke")
 // creates the sroll event
-window.addEventListener('scroll', () =>{
+window.addEventListener('scroll', () => {
     updateImage()
 })
 
-function updateImage(){
+function updateImage() {
     //the opacity starts at 1 and it decrease on scroll 
     bgImageEl.style.opacity = 1 - window.pageYOffset / 800
     bgImageEl.style.backgroundSize = 160 - window.pageYOffset / 12 + "%"
 }
+
+vidEl.onended = () => {
+    setTimeout(() => {
+        vidEl.play();
+
+    }, 10000)
+}
+vidEl.play()
